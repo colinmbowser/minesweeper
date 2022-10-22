d478bd466c48144df306616f0dbd732b2ff65623
Colin Bowser <cbowser@byui.edu>
2022-10-22 05:28:42 -0700

    added new files, doing some tutorials to understand java

---
+++ index.js
@@ -0,0 +1,12 @@
+    // commented comment
+    console.log('Hello World');
+
+    let firstName = 'kevin'; // string
+    const interestRate = .3; // number
+    let isApproved = true; // boolean
+    let lastName; // undefined
+    console.log(firstName);
+    console.log(interestRate);
+    // typeof function used to find what type a variable is. ie typeof firstName;
+
+    let selectedColor = null; // null can reassign color when something is selected
