64b36fc738dbede9673f7562f0fe52bfbd7264b4
Colin Bowser <cbowser@byui.edu>
2022-10-29 21:01:31 -0700

    did code in javascript testing folder, transferring it to real folder

--- index.js
+++ index.js
@@ -1,12 +1,176 @@
-    // commented comment
-    console.log('Hello World');
+export const TILE_STATUS =
+{
+    HIDDEN: 'hidden',
+    MINE: 'mine',
+    NUMBER: 'number',
+    FLAGGED: 'flagged',
+    QUESTION: 'question'
+}
 
-    let firstName = 'kevin'; // string literal
-    const interestRate = .3; // number
-    let isApproved = true; // boolean
-    let lastName; // undefined
-    console.log(firstName);
-    console.log(interestRate);
-    // typeof function used to find what type a variable is. ie typeof firstName;
+export function newField(fieldWidth, fieldHeight, numofMines)
+{
+    const minePositions = getMinePositions(fieldWidth, fieldHeight, numofMines)
+    const mineField = []
 
-    let selectedColor = null; // null can reassign color when something is selected
+    for (let h = 0; h < fieldHeight; h++)
+    {
+        const row = []
+        for (let w = 0; w < fieldWidth; w++)
+        {
+            const element = document.createElement("div")
+            element.dataset.status = TILE_STATUS.HIDDEN
+
+            const tile = 
+            {
+                element,
+                w,
+                h,
+                mine: minePositions.some(positionMatch.bind(null, {w, h})),
+                get status()
+                {
+                    return this.element.dataset.status
+                },
+                set status(value)
+                {
+                    this.element.dataset.status = value
+                }
+            }
+
+            row.push(tile)
+        }
+        mineField.push(row)
+    }
+    return mineField;
+}
+
+export function markTile(tile)
+{
+    if (
+        tile.status !== TILE_STATUS.HIDDEN &&
+        tile.status !== TILE_STATUS.FLAGGED &&
+        tile.status !== TILE_STATUS.QUESTION
+        )
+    {
+        return;
+    }
+
+    if (tile.status === TILE_STATUS.HIDDEN)
+    {
+        tile.status = TILE_STATUS.FLAGGED;
+
+        var flagImg = document.createElement("img");
+        flagImg.src = "flag.png";
+        tile.element.appendChild(flagImg);
+    }
+    else if (tile.status === TILE_STATUS.FLAGGED)
+    {
+        tile.status = TILE_STATUS.QUESTION;
+
+        tile.element.textContent = "?"
+    }
+    else if (tile.status === TILE_STATUS.QUESTION)
+    {
+        tile.status = TILE_STATUS.HIDDEN;
+        tile.element.textContent = "";
+    }
+}
+
+export function revealTile(mineField, tile)
+{
+    if (tile.status !== TILE_STATUS.HIDDEN)
+    {
+        return;
+    }
+
+    if (tile.mine)
+    {
+        tile.status = TILE_STATUS.MINE
+        return
+    }
+
+        tile.status = TILE_STATUS.NUMBER
+
+        const adjacentTiles = scanTiles(mineField, tile)
+        const mines = adjacentTiles.filter(t => t.mine)
+        if (mines.length === 0)
+        {
+            adjacentTiles.forEach(revealTile.bind(null, mineField))
+        }
+        else
+        {
+            tile.element.textContent = mines.length
+        }
+
+}
+
+function getMinePositions(fieldWidth, fieldHeight, numofMines)
+{
+    const positions = []
+
+    while(positions.length < numofMines)
+    {
+        const position =
+        {
+            w: randomNumber(fieldWidth),
+            h: randomNumber(fieldHeight)
+        }
+
+        if (!positions.some(positionMatch.bind(null, position)))
+        {
+            positions.push(position)
+        }
+    }
+    return positions
+}
+
+export function checkWin(mineField)
+{
+    return mineField.every(row =>
+        {
+            return row.every(tile =>
+                {
+                    return tile.status === TILE_STATUS.NUMBER ||
+                    (tile.mine &&
+                        (tile.status === TILE_STATUS.HIDDEN ||
+                            tile.status === TILE_STATUS.FLAGGED))
+                })
+        })
+}
+
+export function checkLose(mineField)
+{
+    return mineField.some(row =>
+        {
+            return row.some(tile =>
+                {
+                    return tile.status === TILE_STATUS.MINE
+                })
+        })
+}
+
+function positionMatch(a, b)
+{
+    return a.w === b.w && a.h === b.h
+}
+
+function randomNumber(sizeVariable)
+{
+    return Math.floor(Math.random() * sizeVariable);
+}
+
+function scanTiles(mineField, {w, h})
+{
+    const tiles = []
+    for (let scanH = -1; scanH <= 1; scanH++)
+    {
+        for (let scanW = -1; scanW <= 1; scanW++)
+        {
+            const tile = mineField[h + scanH]?.[w + scanW]
+            if (tile) 
+            {
+                tiles.push(tile)
+            }
+        }
+    }
+    return tiles
+}
---
+++ script.js
@@ -0,0 +1,104 @@
+
+import { TILE_STATUS, newField, markTile, revealTile, checkWin, checkLose } from "./index.js";
+
+const FIELD_WIDTH = 9;
+const FIELD_HEIGHT = 9;
+const NUMBER_OF_MINES = 10;
+
+//add no right click to gaps of minefield | DONE
+//add flag pixel art to flagged tile | DONE
+//add win condition from having all mines flagged and nothing else flagged | NOT THE WAY YOU PLAY THE GAME
+
+//add choices below to type of game | 
+//easy: 9w 9h 10m
+//medium: 16w 16h 40m
+//hard: 30w 16h 99m
+//custom: userinput w userinput h userinput m
+
+const mineField = newField(FIELD_WIDTH, FIELD_HEIGHT, NUMBER_OF_MINES);
+
+const fieldElement = document.querySelector(".mineField");
+const mineAmountChange = document.querySelector("[data-mine-amount]");
+
+const messageText = document.querySelector(".subtext")
+
+//stops the right click menu from occuring on each tile
+fieldElement.addEventListener("contextmenu", e => { e.preventDefault()})
+
+mineField.forEach(row => 
+{
+    row.forEach(tile =>
+    {
+        fieldElement.append(tile.element);
+        tile.element.addEventListener("click", () =>
+        {
+            revealTile(mineField, tile)
+            checkGameEnd()
+        })
+        tile.element.addEventListener("contextmenu", e =>
+        {
+            e.preventDefault() //stops the right click menu from occuring on each tile
+             markTile(tile)
+             listMinesLeft()
+             checkGameEnd()
+        })
+    })
+})
+
+fieldElement.style.setProperty("--fieldWidth", FIELD_WIDTH);
+fieldElement.style.setProperty("--fieldHeight", FIELD_HEIGHT);
+mineAmountChange.textContent = NUMBER_OF_MINES;
+
+function listMinesLeft()
+{
+    const totalMarkedTiles = mineField.reduce
+    ((count, row) =>
+        { return count + row.filter(tile =>tile.status === TILE_STATUS.FLAGGED).length }, 0)
+    mineAmountChange.textContent = NUMBER_OF_MINES - totalMarkedTiles;
+}
+
+function checkGameEnd()
+{
+    const win = checkWin(mineField)
+    const lose = checkLose(mineField)
+
+    if (win || lose)
+    {
+        fieldElement.addEventListener("click", stopProp, {capture: true})
+        fieldElement.addEventListener("contextmenu", stopProp, {capture: true})
+    }
+
+    if(win)
+    {
+        messageText.textContent = "WINNER!"
+    }
+
+    if (lose)
+    {
+        messageText.textContent = "LOSER!"
+        mineField.forEach(row => 
+            {
+                row.forEach(tile => 
+                    {
+                        if (tile.mine && tile.status === TILE_STATUS.FLAGGED)
+                        {
+                            markTile(tile);
+                            markTile(tile);
+                        }
+                        if (tile.mine && tile.status === TILE_STATUS.QUESTION)
+                        {
+                            markTile(tile);
+                        }
+                        if (tile.mine)
+                        {
+                            revealTile(mineField, tile);
+                        }
+                    })
+            })
+    }
+
+    function stopProp(e)
+    {
+        e.stopImmediatePropagation()
+    }
+}
